ede7e706e57664a4eb633bec9c8f76001eb59f24
Oscar Doel Muñoz <Doncoca4686@gmail.com>
2026-02-02 13:29:13 -0600

    añadido el modelo orders.cs con 4 campos

---
+++ API_V2/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿namespace API_V2.Models
+{
+    public class Orders
+    {
+
+        public int OrderID { get; set; }
+        public string? ShipName { get; set; }
+
+        public int ShipVia { get; set; }
+
+        public decimal Freight { get; set; }    
+
+    }
+}
